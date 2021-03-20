def87aef528576c45c30b14964a5717726526b7e
vvyshko <vvyshko@talend.com>
2021-03-20 23:30:08 +0200

    Added user service global prefix

--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT);
 }
 
